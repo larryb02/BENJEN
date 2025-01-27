a131eb205340dbfdbaf93214992b0beb47fe89bb
Larry Banks <larrykbanks21@gmail.com>
2025-01-27 08:46:34 -0500

    Set up boilerplate for application

--- Sandbox/test_app.cpp
+++ Sandbox/test_app.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
 #include <filesystem>
+#include "../Benjen/src/core/Application.h"
+#include <chrono>
+#include <thread>
 /*
-    Entry Point for Application 
+    Entry Point for Application
     This code will create a runtime for your game/application
     Pseudo Boilerplate:
         init systems
 */
 
-/*
-    class Sandbox : public BENJEN::Application 
-    {
+class Sandbox : public BENJEN::Application
+{
+    void OnUpdate();
+};
 
-    }
+void Sandbox::OnUpdate()
+{
+    std::cout << "Updating!!" << std::endl;
+    // sleep
+    std::this_thread::sleep_for(std::chrono::milliseconds(5));
+}
 
-    Sandbox::OnUpdate()
-    {
-        
-    }
-*/
-
-//int main(int argc, char **argv)
-// {
-        // init
-        // Application.Run()
-        // Application.Exit() maybe
-//     return EXIT_SUCCESS;
-// }
+int main(int argc, char **argv)
+{
+    Sandbox *sb = new Sandbox();
+    sb->Run();
+    delete sb;
+    return EXIT_SUCCESS;
+}
